bc26a639d4f867df03583442489139197b2fdfe5
Sujoy Dhar <sujoydhar95@gmail.com>
2024-01-12 09:19:55 +0000

    app.py : simple san processor added

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -9,9 +9,13 @@ from opentelemetry import trace
 from opentelemetry.instrumentation.flask import FlaskInstrumentor
 from opentelemetry.instrumentation.requests import RequestsInstrumentor
 from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import ConsoleSpanExporter
+
+
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
-
+from opentelemetry.sdk.trace.export import SimpleSpanProcessor
 
 from services.home_activities import *
 from services.notification_activities import *
@@ -29,6 +33,11 @@ from services.show_activity import *
 provider = TracerProvider()
 processor = BatchSpanProcessor(OTLPSpanExporter())
 provider.add_span_processor(processor)
+
+simple_processor = SimpleSpanProcessor(ConsoleSpanExporter())
+simple_provider.add_span_processor(simple_processor)
+
+
 trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 
