d14bdaad94e72b3179457319ff25e241be18347f
Sujoy Dhar <sujoydhar95@gmail.com>
2024-01-13 16:00:44 +0000

    gitpod.yaml: npm i added,x-ray configs added

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -4,6 +4,10 @@ from flask_cors import CORS, cross_origin
 import os
 
 
+#x-ray imports
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
+
 # honeycomb imports
 from opentelemetry import trace
 from opentelemetry.instrumentation.flask import FlaskInstrumentor
@@ -51,6 +55,11 @@ app = Flask(__name__)
 FlaskInstrumentor().instrument_app(app)
 RequestsInstrumentor().instrument()
 
+#x-Ray 
+
+xray_url = os.getenv("AWS_XRAY_URL")
+xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
+XRayMiddleware(app, xray_recorder)
 
 
 
