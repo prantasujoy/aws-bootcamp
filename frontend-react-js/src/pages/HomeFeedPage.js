a142fd57872cad5ef0e8f92a501535cefdf8c908
Sujoy Dhar <sujoydhar95@gmail.com>
2024-02-11 18:10:26 +0000

    aws-amplify rollback to v3

--- frontend-react-js/src/pages/HomeFeedPage.js
+++ frontend-react-js/src/pages/HomeFeedPage.js
@@ -7,7 +7,7 @@ import ActivityFeed from "../components/ActivityFeed";
 import ActivityForm from "../components/ActivityForm";
 import ReplyForm from "../components/ReplyForm";
 
-import { Auth } from "aws-amplify";
+import {Auth} from 'aws-amplify'
 
 // [TODO] Authenication
 import Cookies from "js-cookie";
